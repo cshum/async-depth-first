95fbb9458f79e9a7da5916a921e6296c8ee077ac
Adrian Shum <me@cshum.com>
2015-10-23 11:45:40 +0800

    clean up

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var semaphore = require('sema')
 
 function Async () {
   if (!(this instanceof Async)) return new Async()
-  this._q = this._q || [semaphore(1)]
+  this._q = [semaphore(1)]
   this._error = null
 }
 
